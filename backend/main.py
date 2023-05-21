7fa0f4e3b446a3ac7ee8e3bd69a55144f25118b1
Jonas Senft-Raiss <jonas.senftraiss@gmail.com>
2023-05-21 15:31:52 +0100

    Frontend API call on flask predict backend works now

--- backend/main.py
+++ backend/main.py
@@ -2,17 +2,22 @@ from flask import Flask, request, jsonify, render_template
 import joblib
 from sklearn.feature_extraction.text import TfidfVectorizer
 import numpy as np
+from flask_cors import CORS
 
 import logging
 
 app = Flask(__name__)
+CORS(app)
 
 # load the pre-trained model and vectorizer
 model, vectorizer = None, None
 
+
 @app.route("/")
 def home():
-    return '<h1>Text Prediction</h1>'
+    return "<h1>Text Prediction</h1>"
+
+
 #     return """<!DOCTYPE html>
 # <html>
 # <head>
@@ -29,38 +34,42 @@ def home():
 # </html>
 #     """
 
-@app.route("/predict", methods=["POST"])
+
+@app.route("/predict", methods=["POST", "OPTIONS"])
 def predict():
+    # return "Hello cross-origin-world!"
     # initialize the data dictionary that will be returned from the view
     data = {"success": False}
 
     # ensure text was properly submitted to our endpoint
     if request.method == "POST":
-        if request.form.get("text"):
-            # read the text from the form
-            text = request.form.get("text")
-            # since we are using just one text sample, we need to convert it to a list and then to a numpy array for vectorization
+        # if request.form.get("text"):
+        # read the text from the form
+        # text = request.form.get("text")
+        text = request.get_json()["inputField"]
+        print(text)
+        # since we are using just one text sample, we need to convert it to a list and then to a numpy array for vectorization
 
-            try:
-                # preprocess the text
-                text_vector = vectorizer.transform([text])
+        try:
+            # preprocess the text
+            text_vector = vectorizer.transform([text])
 
-                # classify the input text and then initialize the list of predictions to return to the client
-                prediction = model.predict(text_vector)
-                data["prediction"] = str(prediction[0])
+            # classify the input text and then initialize the list of predictions to return to the client
+            prediction = model.predict(text_vector)
+            data["prediction"] = str(prediction[0])
 
-                # indicate that the request was a success
-                data["success"] = True
+            # indicate that the request was a success
+            data["success"] = True
 
-            except Exception as e:
-                # log the error
-                logging.error(str(e))
+        except Exception as e:
+            # log the error
+            logging.error(str(e))
 
-                # indicate that an error occurred
-                data["error"] = "An error occurred while making the prediction."
+            # indicate that an error occurred
+            data["error"] = "An error occurred while making the prediction."
 
     # return the data dictionary as a JSON response
-    return jsonify(data)
+    return data
 
 
 def load_model():
@@ -68,14 +77,20 @@ def load_model():
     # load the pre-trained model
     model = joblib.load("model/pa_model.pkl")
 
+
 def load_vectorizer():
     global vectorizer
     # load the pre-trained vectorizer
     vectorizer = joblib.load("model/vectorizer.pkl")
 
+
 if __name__ == "__main__":
-    print(("* Loading model and starting Flask server..."
-        "please wait until the server has fully started"))
+    print(
+        (
+            "* Loading model and starting Flask server..."
+            "please wait until the server has fully started"
+        )
+    )
     # load the model and vectorizer
     load_model()
     load_vectorizer()
--- frontend/vite-react-app/src/App.tsx
+++ frontend/vite-react-app/src/App.tsx
@@ -1,9 +1,77 @@
 import { useState } from 'react'
 
 function App() {
+
+  const [inputField, setInputField] = useState<string>("");
+  const [output, setOutput] = useState<string>("Initializer");
+
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+    console.log(inputField);
+
+    // setOutput("Loading...")
+
+    // send inputField to Flask backend
+    // const response = await fetch("http://127.0.0.1:5000/predict", {
+    //   mode: "no-cors",
+    //   method: "POST",
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //     },
+    //     body: JSON.stringify({ inputField }),
+    //   });
+
+    //   // get response from Flask backend
+    //   const data = await response.json();
+    //   setOutput(data.output);
+
+    // fetch ("http://127.0.0.1:5000/predict", {
+    //   method: "POST",
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //     },
+    //     body: JSON.stringify({ inputField }),
+    //   })
+    //   .then(response => response.json())
+    //   .then(data => setOutput(data.output))
+    //   .catch(error => console.log(error))
+
+    const response = fetch ("http://127.0.0.1:5000/predict", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ inputField }),
+      })
+      const data = await response.then(response => response.json());
+      console.log(data);
+      console.log("calling setOutput");
+      console.log(data.error);
+      if (data.error) {
+        setOutput(data.error);
+      }
+      else {
+        setOutput(data.prediction);
+      }
+
+
+
+      // setInputField("");
+
+
+  }
+
   return (
     <div>
-      <h1>Hello Vite + React!</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={inputField}
+          onChange={(e) => setInputField(e.target.value)}
+        />
+        <button type="submit">Submit</button>
+      </form>
+      <p>{output}</p>
     </div>
   );
 }
